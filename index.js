5cb668754243132bb49e071b7fc5e53124b596b5
unknown <obirikan020@gmail.com>
2021-11-07 10:32:14 -0800

    just finished with the footer

---
+++ index.js
@@ -0,0 +1,27 @@
+var ul=document.getElementsByTagName("ul")
+var input=document.getElementById("input")
+var li=document.getElementsByTagName("li")
+
+           //using ES5
+//adding event listner
+input.addEventListener('keyup',kay)
+
+function kay(){
+ let value=document.getElementById('input').value.toUpperCase();
+   //getting ul
+ var ul=document.getElementById('names')
+   //getting li from ul
+ var li=ul.querySelectorAll('li.list')
+   //looping 
+ for(var i=0;i<li.length;i++){
+   a=li[i].getElementsByTagName('a')[0];
+   if(a.innerHTML.toUpperCase().indexOf(value)>-1){
+     li[i].style.display=""
+   }else{
+    li[i].style.display="none" 
+   }
+ }
+ 
+}
+        //using ES6
+ 
